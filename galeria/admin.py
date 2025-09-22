f69c4e2689f0f8350ff0d34c5849b224c346da4d
Guilherme Soato <guilhermesoato7@gmail.com>
2025-09-22 15:07:55 -0300

    alterações no banco de dados e construção do admin do servidor

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'id', 'legenda')
+    list_per_page = 20
+
+admin.site.register(Fotografia, ListandoFotografias)
+
