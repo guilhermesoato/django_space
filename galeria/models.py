491b157f70ae179cfadfece71dfc9f92238a1330
Guilherme Soato <guilhermesoato7@gmail.com>
2025-09-22 15:31:34 -0300

    acréscimo de detalhamento de publicação e efetuação de makemigrations

--- galeria/models.py
+++ galeria/models.py
@@ -6,15 +6,15 @@ class Fotografia(models.Model):
         ("NEBULOSA", "Nebulosa"),
         ("ESTRELA", "Estrela"),
         ("GALÁXIA", "Galáxia"),
-        ("PLANETA", "Planeta")
+        ("PLANETA", "Planeta"),
     ]
     
-
     nome = models.CharField(max_length=100, null = False, blank = False)
     legenda = models.CharField(max_length=255, null = False, blank = False)
     categoria = models.CharField(max_length=100, choices = OPCOES_CATEGORIA, default = '')
     descricao = models.TextField(null = False, blank = False)
     foto = models.CharField(max_length=100, null = False, blank = False)
+    publicada = models.BooleanField(default=False)
     
     def __str__(self):
         return f"Fotografia: [nome={self.nome}]"
